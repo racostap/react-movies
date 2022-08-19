2cbcec009e5624b871d73e73996792ceba88996f
Racostapinto <racostapinto@gmail.com>
2022-08-18 23:02:40 -0500

    routing con error

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,8 +1,18 @@
-import { MovieCard } from './MovieCard';
-import movies from './movies.json';
-import styles from './MoviesGrid.module.css';
+
+import { useEffect, useState } from "react";
+import { get } from "../utils/httpClient";
+import { MovieCard } from "./MovieCard";
+import styles from "./MoviesGrid.module.css";
 
 export function MoviesGrid() {
+    const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    get("/discover/movie").then((data) => {
+      setMovies(data.results);
+    });
+  }, []);
+    
     return (
       
         <ul className={styles.moviesGrid}>
